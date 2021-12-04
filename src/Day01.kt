a513af095c842678095854f5311dbb588ffba242
Ashley <dresswithpockets@pm.me>
2021-12-03 17:31:35 -0700

    day 1 part 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,13 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var prev: Int? = null
+        fun checkAndSet(d: Int): Boolean {
+            var greater = (prev ?: Int.MAX_VALUE) < d
+            prev = d
+            return greater
+        }
+        val count =  input.map { s -> s.toInt() }.count { d -> checkAndSet(d) }
+        return count
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +16,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
+    //println(part2(input))
 }
